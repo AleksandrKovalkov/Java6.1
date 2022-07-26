73e5359b42b3ce5e2b9bcdb540aa324e32605ee8
Ковальков Александр <akovalkoff@yandex.ru>
2022-07-26 12:22:53 +0300

    SQRService

---
+++ src/test/java/ru/netology/JavaCycles/SqrServiceTest.java
@@ -0,0 +1,71 @@
+package ru.netology.JavaCycles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SQRServiceTest {
+    SQRService sqrService = new SQRService();
+
+    @Test
+    void calculateSQR() {
+        int expected = 3;
+        int actual = sqrService.calculateSQR(200, 300);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateTheNumberOfSquaresExceedingTheMinimumLimit() {
+
+        int expected = 0;
+        int minNumber = 10;
+        int maxNumber = 99;
+
+        int actual = sqrService.calculateSQR(minNumber, maxNumber);
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void shouldCalculateTheMinimumNumberOfSquares() {
+        SQRService service = new SQRService();
+
+        int expected = 1;
+        int minNumber = 100;
+        int maxNumber = 100;
+
+        int actual = sqrService.calculateSQR(minNumber, maxNumber);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldCalculateTheMaximumNumberOfSquares() {
+        SQRService service = new SQRService();
+
+        int expected = 90;
+        int minNumber = 100;
+        int maxNumber = 9801;
+
+        int actual = sqrService.calculateSQR(minNumber, maxNumber);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldCalculateTheNumberOfSquaresExceedingTheMaximumLimit() {
+        SQRService service = new SQRService();
+
+        int expected = 90;
+        int minNumber = 100;
+        int maxNumber = 100_000;
+
+        int actual = sqrService.calculateSQR(minNumber, maxNumber);
+
+        assertEquals(expected, actual);
+    }
+}
